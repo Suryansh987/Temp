845493da5317a9ea8573b01538fbf09ff82fc5d9
Suryansh987 <suryanshsharma987@gmail.com>
2023-06-06 20:59:07 +0530

    Add files via upload

---
+++ SEARCHENGINE/setting.py
@@ -0,0 +1,8 @@
+SEARCH_KEY = "enter your api key"
+SEARCH_ID = "enter the cx from script link"
+COUNTRY = 'enter you region code'
+
+SEARCH_URL="https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+RESULT_COUNT = 20
+
+
---
+++ SEARCHENGINE/test.py
@@ -0,0 +1,4 @@
+import html
+s='01-Nov-2020 <b>...</b> Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on <b>YouTube</b>'
+d=html.escape(s)
+print(d)
